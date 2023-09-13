a581196ce9354208a093e3438242af113b958cca
Matias <sanchezmatias968@gmail.com>
2023-09-12 21:17:03 -0400

    refrescar cambios

--- src/app/categorias/categorias.component.ts
+++ src/app/categorias/categorias.component.ts
@@ -1,5 +1,3 @@
-// src/app/categorias/categorias.component.ts
-
 import { Component, OnInit } from '@angular/core';
 import { Categoria } from '../model/categoria.model';
 import { CategoriaService } from '../service/categoria.service';
@@ -16,6 +14,10 @@ export class CategoriasComponent implements OnInit {
   constructor(private categoriaService: CategoriaService) {}
 
   ngOnInit(): void {
+    this.cargarCategorias();
+  }
+
+  cargarCategorias(): void {
     this.categorias = this.categoriaService.getCategorias();
   }
 
@@ -23,15 +25,22 @@ export class CategoriasComponent implements OnInit {
     if (this.nuevaCategoria.trim() !== '') {
       this.categoriaService.agregarCategoria(this.nuevaCategoria);
       this.nuevaCategoria = '';
+
+      // Después de agregar una categoría, vuelves a cargar la lista de categorías
+      this.cargarCategorias();
     }
   }
 
   editarCategoria(id: number, nuevaDescripcion: string): void {
     this.categoriaService.editarCategoria(id, nuevaDescripcion);
+
+    // Después de editar una categoría, vuelves a cargar la lista de categorías
+    this.cargarCategorias();
   }
 
   eliminarCategoria(id: number): void {
     this.categoriaService.eliminarCategoria(id);
+    // Después de eliminar una categoría, vuelves a cargar la lista de categorías
+    this.cargarCategorias();
   }
 }
-
