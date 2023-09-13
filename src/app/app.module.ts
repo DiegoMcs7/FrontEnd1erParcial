ee5a729f5af4f030710b1d6bce6cb07afb330dd6
DiegoMcs7 <diegoespinola98@fpuna.edu.py>
2023-09-12 20:55:50 -0400

    1era solución

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,28 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { CategoriaService } from "./service/categoria.service";
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { CategoriasComponent } from './categorias/categorias.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [AppComponent, CategoriasComponent],
-  imports: [BrowserModule, FormsModule, RouterModule],
-  providers: [CategoriaService],
-  bootstrap: [AppComponent],
+  declarations: [
+    AppComponent,
+    CategoriasComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    RouterModule,
+    HttpClientModule
+  ],
+  providers: [
+    CategoriaService
+  ],
+  bootstrap: [
+    AppComponent
+  ],
 })
 export class AppModule {}
