127eea63c5de35a84c0277bc29f0d2102cb7dccc
Denis <denis03esquivel@gmail.com>
2023-09-18 21:50:29 -0400

    'pdf'

--- src/app/ficha/ficha.component.ts
+++ src/app/ficha/ficha.component.ts
@@ -4,7 +4,9 @@ import { Ficha } from '../model/fichas';
 import { Persona } from '../model/paciente_doctor.model';
 import { CategoriaService } from "../service/categoria.service";
 import { ServicefichaService } from '../service/serviceficha.service';
-import { PersonaService } from '../service/paciente_doctor.service'
+import { PersonaService } from '../service/paciente_doctor.service';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas'
 
 type Filtro = {
   fechaDesde ?: string,
@@ -87,6 +89,31 @@ export class FichaComponent implements OnInit {
   getCategorias() {
     this.serviceCategoria.getCategorias().subscribe((data: any) => {
       this.categorias = data.lista;
+    });
+  }
+
+  downloadPDF() {
+    // Extraemos el
+    const DATA: any = document.getElementById('htmlData');
+    const doc = new jsPDF('p', 'pt', 'a4');
+    const options = {
+      background: 'white',
+      scale: 3
+    };
+    html2canvas(DATA, options).then((canvas) => {
+
+      const img = canvas.toDataURL('image/PNG');
+
+      // Add image Canvas to PDF
+      const bufferX = 15;
+      const bufferY = 15;
+      const imgProps = (doc as any).getImageProperties(img);
+      const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
+      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+      doc.addImage(img, 'PNG', bufferX, bufferY, pdfWidth, pdfHeight, undefined, 'FAST');
+      return doc;
+    }).then((docResult) => {
+      docResult.save(`${new Date().toISOString()}_tutorial.pdf`);
     });
   }
 
---
+++ src/jspdf.d.ts
@@ -0,0 +1,6 @@
+
+declare module 'jspdf' {
+    const jsPDF: any; // Puedes ajustar el tipo 'any' si conoces la estructura de tipos adecuada
+    export default jsPDF;
+  }
+  
